db23dbcf31b879ba4cdb5de7289f9cba49693ab2
Sunilg66 <sunilkg251@gmail.com>
2021-06-01 17:09:25 +0530

    adding file

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+class Factorial{  
+public static void main(String args[]){  
+  
+int j =1;  
+int num=4;    
+for(int i=1;i<=num;i++){    
+j=j*i;    
+}    
+System.out.println("Factorial of "+num+" is: "+j);    
+}  
+}
